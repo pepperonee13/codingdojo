a24894e22998d631094f5f6de6e3f407b80c22f5
Peter Szarvas <peter.szarvas@softwarepark.cc>
2019-10-04 15:54:21 +0200

    Test refactored

--- test.js
+++ test.js
@@ -2,21 +2,26 @@ const assert = require('assert');
 const fizzBuzzResult = require("./result");
 const { compute } = require('./fizzbuzz');
 
+function assertLastItemEquals(computeMaxValue, expectedValue){
+
+    assert.equal(compute(computeMaxValue)[computeMaxValue - 1], expectedValue);
+}
+
 describe('FizzBuzz tests', function () {
-    it('should return correct string for 5', function () {
-        assert.deepEqual(['1', '2', 'Fizz', '4', 'Buzz'], compute(5));
-    });
     it('should return FizzBuzz for every number which can be divided with 3 and 5', function () {
-        const computeCollection = compute(15);
-        assert.equal(computeCollection[14], 'FizzBuzz');
+        assertLastItemEquals(15, "FizzBuzz");
+    });
+    it('should return Fizz for 3', function () {
+        assertLastItemEquals(3, "Fizz");
+    });
+    it('should return Buzz for 5', function () {
+        assertLastItemEquals(5, "Buzz");
     });
     it('should be Fizz if contains 3', function () {
-        const computeCollection = compute(13);
-        assert.equal(computeCollection[12], 'Fizz');
+        assertLastItemEquals(13, "Fizz");
     });
     it('should be Buzz if contains 5', function () {
-        const computeCollection = compute(52);
-        assert.equal(computeCollection[51], 'Buzz');
+        assertLastItemEquals(52, "Buzz");
     });
 
     it("should work up to 36 numbers", () => {
